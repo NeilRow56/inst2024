7d140628777223878e0663bc55955b8ccaf33d80
NeilRow56 <neilrowland56@gmail.com>
2024-02-15 06:26:07 +0000

    Added Sidebar with nanlinks, logo and dropdown with theme changer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true} className="dark">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
